605a70efa468062f06e6b82c57a77dc0bf21e80f
AINAN SALSABILLA <117760154+ayienan@users.noreply.github.com>
2023-01-26 11:04:17 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1,19 +1,14 @@
 import socket
 
 def main():
-    # create socket
     sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
-    # connect to server on the port.
     sc.connect(("192.168.56.1", 8080))                               
 
-    # prompt user to input the temp in fahrenheit
     temp_F = input("Please enter temperature in Fahrenheit: ")
     sc.send(temp_F.encode())
     
-    # receive response from server then print output
     temp_C = sc.recv(1024).decode()
     print("The temperature in Celsius is:",temp_C)
 
-# execute main func
 main()
